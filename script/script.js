8877a5afe9a832a33f50f05ef3345f957a096ae2
Yang Muhe <yangmuhe@Yangs-MBP.home>
2015-10-30 09:01:57 -0400

    Completed, but there're some parts I'm not sure

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 var margin = {t:50,r:125,b:50,l:125};
-var width = document.getElementById('plot') - margin.r - margin.l,
-    height = document.getElementById('plot') - margin.t - margin.b;
+var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
+    height = document.getElementById('plot').clientHeight - margin.t - margin.b;
 
 var canvas = d3.select('.plot')
     .append('svg')
@@ -18,24 +18,51 @@ d3.csv('data/olympic_medal_count.csv', parse, dataLoaded);
 
 function dataLoaded(err,rows){
 
-    var year = 1900;
-    rows.sort(function(a,b){
-        //Note: this is called a "comparator" function
-        //which makes sure that the array is sorted from highest to lowest
-        return b[year] - a[year];
-    });
+    /*
+     var year = 1900;
+     rows.sort(function(a,b){
+     //Note: this is called a "comparator" function
+     //which makes sure that the array is sorted from highest to lowest
+     return b[year] - a[year];
+     });
 
-    //Note: this returns "top5" as a subset of the larger array "rows", containing positions 0,1,2,3,4
-    var top5 = rows.slice(0,5);
+     //Note: this returns "top5" as a subset of the larger array "rows", containing positions 0,1,2,3,4
+     var top5 = rows.slice(0,5);
 
-    //Call the draw function
-    draw(top5, year);
+     //Call the draw function
+     draw(top5, year);
+     */
 
     //TODO: fill out this function
     d3.selectAll('.btn-group .year').on('click',function(){
 
-        var year = d3.select(this).id();
+        var year = d3.select(this).attr('id');  //this line is necessary even if the value of year is defined in the if/else
+
         console.log("Show top 5 medal count for: " + year);
+
+        if(year=='year-1900'){
+            var year = 1900
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            })
+            var top5 = rows.slice(0,5);
+            draw(top5, year);   //Call the draw function
+        }else if(year=='year-1960'){
+            var year = 1960
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            })
+            var top5 = rows.slice(0,5);
+            draw(top5, year);
+        }else{
+            var year = 2012
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            })
+            var top5 = rows.slice(0,5);
+            draw(top5, year);
+        }
+
     });
 }
 
@@ -43,6 +70,57 @@ function draw(rows, year){
     //TODO: Complete drawing function, accounting for enter, exit, update
     //Note that this function requires two parameters
     //The second parameter, "year", determines which one of the three years (1900,1960,2012) to draw the medal counts based on
+
+    var nodes = canvas.selectAll('.country')
+        .data(rows, function(d){return d.country});
+
+    //enter
+    var nodesEnter = nodes.enter()
+        .append('g')
+        .attr('class','country')
+        .attr('transform',function(d,i){
+            return 'translate('+((width/4)*i)+','+0+')'
+        })     //without this line the circles would appear from the top-left corner
+
+    nodesEnter.append('circle')
+        .attr('r',0)
+        .style('fill','rgba(6,114,237,.1)')
+        .style('stroke','rgb(50,50,50)')
+        .style('stroke-width','1px');
+
+    nodesEnter.append('text')
+        .text(function(d){return d.country;})
+        .attr('text-anchor','middle')
+        .style('fill','rgb(50,50,50)')
+        .style('font-size','12px')
+        //.attr("y", 0);   //? This determines the relative(?) y-position of the text with the center of the circles.
+                           //It works the same without this line, so it seems that 0 is the default value?
+
+    nodesEnter.append('text')
+        .text(function(d){return d[year];})
+        .attr('text-anchor','middle')
+        .style('fill','rgb(50,50,50,)')
+        .style('font-size','10px')
+        .attr("y", 15);
+
+    //exit
+    nodes.exit()
+        .remove();
+
+    //update
+    nodes
+        .transition()
+        .duration(1000)
+        .attr('transform',function(d,i){
+            return 'translate('+(width/4)*i+',100)';  //the y value here should be a number different from that in "enter"
+        })
+        .select('circle')
+        .attr('r',function(d){return scaleR(d[year])})   //IMPORTANT!! "[]" are used here because it's an array.
+                                                        // But how does it relate "year" to 1900/1960/2012?
+
+
+
+
 }
 
 function parse(row){
